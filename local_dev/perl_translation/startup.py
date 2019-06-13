7c60f83c60dee289e392a1f5bbc72a9d425ff45e
allanrab <benjamin.allan-rahill@bms.com>
2019-06-13 12:41:39 -0400

    attempted error handling on startup uid parsing

--- local_dev/perl_translation/startup.py
+++ local_dev/perl_translation/startup.py
@@ -44,8 +44,12 @@ if __name__ == "__main__":
     uid = re.sub('\D','',uid)
 
     gid = call('id -g domino')
-    uid = re.sub('\D','',gid)
 
+    try:
+        uid = re.sub('\D','',gid)
+    except TypeError as terror:
+        print("The UID IS: \n")
+        raise terror
     call("mkdir -p /home/domino/{}".format(current_user))
     call("sshfs {0}\@stash.pri.bms.com:/stash /stash -o IdentityFile=/home/domino/.ssh/id_rsa_{0} " \
             "-o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o uid={1} -o gid={2}".format(current_user, uid, gid))
