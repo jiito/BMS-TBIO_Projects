32befb15ea137f1a4fc054485ae3b2323d298f86
allanrab <benjamin.allan-rahill@bms.com>
2019-07-08 13:03:28 -0400

    added versions to print statement

--- docker_dev/json-dockerfile/json-dockerfile.py
+++ docker_dev/json-dockerfile/json-dockerfile.py
@@ -2,8 +2,12 @@
 
 import json
 
+atp_packages = []
+r_packages = []
+
+
 with open("json-dockerfile.json", "r") as f:
     data = json.load(f)
 
     for i in data["apt"]:
-        print(i["name"])
+        print("{}={}".format(i["name"], i["version"]))
