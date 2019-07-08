f2ce01c7c35e2116474f958e89207b8aeced23ef
allanrab <benjamin.allan-rahill@bms.com>
2019-07-08 13:06:49 -0400

    added KeyError handling to deal with no versions

--- docker_dev/json-dockerfile/json-dockerfile.py
+++ docker_dev/json-dockerfile/json-dockerfile.py
@@ -10,4 +10,9 @@ with open("json-dockerfile.json", "r") as f:
     data = json.load(f)
 
     for i in data["apt"]:
-        print("{}={}".format(i["name"], i["version"]))
+        try:
+            name = i["name"]
+            version = i["version"]
+            print("{}={}".format(i["name"], i["version"]))
+        except KeyError as no_version:
+            print(name)            
